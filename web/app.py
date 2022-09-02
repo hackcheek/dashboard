5a96b74c6cad2efdb752ecdcf4d57348fbbb53d0
hackcheek <h4ckcheek@gmail.com>
2022-09-02 19:46:37 -0300

    testing

--- web/app.py
+++ web/app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -5,8 +7,12 @@ from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
+
+web_dir = os.path.dirname(__file__)
+path = os.path.join(web_dir, "static/")
+
+app.mount("/static", StaticFiles(directory=path), name="static")
 templates = Jinja2Templates(directory='templates')
-app.mount('/static', StaticFiles(directory='static'), name='static')
 
 @app.get('/', response_class=HTMLResponse)
 async def input_page(request: Request):
