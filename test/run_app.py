7d4f68ca49eacf3ca0ea56b2c46ce00124523acf
hackcheek <h4ckcheek@gmail.com>
2022-09-02 22:39:25 -0300

    data json

--- test/run_app.py
+++ test/run_app.py
@@ -4,4 +4,4 @@ from web import app
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8080)   
+    uvicorn.run(app, host='127.0.0.1', port=8000)   
--- web/app.py
+++ web/app.py
@@ -1,19 +1,34 @@
 import os
+import plotly.express as px
 
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.responses import HTMLResponse
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import HTMLResponse, JSONResponse
+
 
 app = FastAPI()
 
 
 web_dir = os.path.dirname(__file__)
-path = os.path.join(web_dir, "static/")
+static_path = os.path.join(web_dir, "static/")
+templates_path = os.path.join(web_dir, "templates/")
 
-app.mount("/static", StaticFiles(directory=path), name="static")
-templates = Jinja2Templates(directory='templates')
+app.mount("/static", StaticFiles(directory=static_path), name="static")
 
-@app.get('/', response_class=HTMLResponse)
-async def input_page(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+templates = Jinja2Templates(directory=templates_path)
+
+
+# @app.get('/', response_class=HTMLResponse)
+# async def helloworld(request: Request):
+#     return templates.TemplateResponse('index.html', {'request': request})
+data = px.data.stocks()
+
+@app.get('/', response_class=JSONResponse)
+async def plotly_data():
+    fig = px.line(data['AAPL'])
+    response = jsonable_encoder(fig.to_json())
+    return JSONResponse(
+        content=response
+    )
